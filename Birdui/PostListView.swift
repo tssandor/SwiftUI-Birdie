8ce327f7a5d8ee03959e73c4f5026573be6f52a2
tssandor <Hello@sandor.im>
2020-07-12 16:57:17 +0800

    Modal presented

--- Birdui/PostListView.swift
+++ Birdui/PostListView.swift
@@ -11,24 +11,35 @@ import SwiftUI
 struct PostListView: View {
   
   @ObservedObject var posts = PostViewModel()
+  @State var modalIsPresented = false
   
   var body: some View {
     // TODO: This should look exactly like the Birdie table view,
     // but with only one button.
     VStack(alignment: .leading) {
+      
       HStack {
         Image("mascot_swift-badge")
           .resizable()
           .frame(width: 80.0, height: 80.0)
-  //      Spacer()
+
         Spacer()
+
         Text("Home")
           .multilineTextAlignment(.center)
+
         Spacer()
       }
-      Button(action: {}) {
+      
+      Button(
+        action: { self.modalIsPresented = true }
+      ) {
         Text("Create New Post")
       }
+      .sheet(isPresented: $modalIsPresented) {
+        NewPostView(postHandler: PostViewModel())
+      }
+      
       List(self.posts.posts.indices) { item in
         PostView(post: self.posts.posts[item])
       }
